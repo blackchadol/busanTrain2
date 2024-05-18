22cb70b336bdb2d02813151c1c15abe3e4900542
blackchadol <ojs831574@gmail.com>
2024-05-18 23:02:51 +0900

    4.5에 preZloc 변수 최신화 누락 확인 후 추가

--- busanTrain2.c
+++ busanTrain2.c
@@ -42,6 +42,104 @@ int getLenght(void);
 int getStamina(void);
 int getProb(void);
 void printTrain(int,int,int,int);
+int verifyAggro(int);
+int verifyStamina(int);
+int getMmove(int, int);
+
+void AsciiArt(void)  //  ASCII art를 이용한 인트로 출력함수
+{
+	printf("                 ()\n");
+	printf("                 ()\n");
+	printf("                 ()       ____.______._____\n");
+	printf("	      .--.  ---- - | _ - a:f - |\n");
+	printf("	  __ || ___ | [_]| |. | #  | .[].[].[].. |\n");
+	printf("	   o)__ | _ | .. |= | _ | -| ___________ |\n");
+	printf("	  __ < (__(*)_(*)_~_____~(*)____(*)_____\n");
+	printf("          ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n");
+
+	printf("                 TRAIN TO BUSAN\n");
+	printf("\n\n\n\n");
+}
+
+int getLenght(void)     //  기차길이 입력을 받는 함수
+{
+	int nInput;
+	while (1)
+	{
+		printf("train lenght(15~50)>>");
+		scanf_s("%d", &nInput);
+		if (nInput >= LEN_MIN && nInput <= LEN_MAX)
+		{
+			break;
+		}
+
+	}
+	return nInput;
+}
+
+int getStamina(void)      // stm 입력을 받는 함수
+{
+	int nInput;
+	while (1)
+	{
+		printf("madongseok stamina(0~5)>>");
+		scanf_s("%d", &nInput);
+		if (nInput >= STM_MIN && nInput <= STM_MAX)
+		{
+			break;
+		}
+
+	}
+	return nInput;
+}
+
+int getProb(void)     //  확률 입력을 받는 함수
+{
+	int nInput;
+	while (1)
+	{
+		printf("percentile probability 'p'(10~90) >> ");
+		scanf_s("%d", &nInput);
+		if (nInput >= PROB_MIN && nInput <= PROB_MAX)
+		{
+			break;
+		}
+
+	}
+	return nInput;
+}
+
+void printTrain(int cLoc, int zLoc, int mLoc, int length) // 열차상태 출력함수
+{
+
+	for (int k = 0; k < length + 1; k++) // 열차 상태 배열에서 모두 공백으로 초기화 한 후 끝에 null문자로 문자열 종료알림
+	{
+		g_trainList[k] = ' ';
+	}
+	g_trainList[length] = '\0';
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (i == 1) // 2열에 C , Z , M 배치 후  출력
+		{
+			g_trainList[0] = '#';
+			g_trainList[length - 1] = '#';
+			g_trainList[cLoc] = 'C';
+			g_trainList[zLoc] = 'Z';
+			g_trainList[mLoc] = 'M';
+			printf("%s\n", g_trainList);
+		}
+
+		else
+		{
+			for (int j = 0; j < length; j++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
 int verifyAggro(int aggro)
 {
 	if (aggro > AGGRO_MAX)
@@ -77,7 +175,7 @@ int getMmove(int zLoc, int mLoc) // 마동석의 move left 또는 move stay 입력을 받�
 				continue;
 			}
 		}
-		
+
 		else
 		{
 			printf("madongseok move(0:stay, 1:left)>> ");
@@ -110,11 +208,12 @@ int verifyStamina(int stm)
 
 
 
+
 int main(void)
 {
-	
+
 	// 1. ASCII ART 출력
-	AsciiArt(); 
+	AsciiArt();
 
 	// 2. 사용자 입력 받기 및 열차상태출력
 	int length = 0, prob = 0, stm = 0; // 열차길이, 확률, 스태미너 변수 선언 및 초기화.
@@ -143,8 +242,8 @@ int main(void)
 	int zombiePull = 0; // 마동석이 action pull 했는지 
 	int mAction = 0; // 마동석 행동 
 	srand((unsigned int)time(NULL));
-	
-	
+
+
 	while (1)
 	{
 		++turn; //1턴부터 시작. 
@@ -163,11 +262,11 @@ int main(void)
 		{
 			--cAggro;
 			cAggro = verifyAggro(cAggro);
-			if (turn % 2 != 0 && zombiePull != 1) // 마동석이 PULL을 하지 않았을 때만 움직임. 
+			if (turn % 2 != 0 && zombiePull != 1) // 홀수턴에 마동석이 PULL을 하지 않았을 때만 움직임. 
 			{
-				if (cAggro > mAggro && zLoc - cLoc != 1)
+				if (cAggro > mAggro && zLoc - cLoc != 1)// 시민의 어그로가 더 높지만 인접하지 않을 때 시민쪽으로 움직임
 					--zLoc;
-				else if(cAggro <= mAggro && mLoc - zLoc != 1)
+				else if (cAggro <= mAggro && mLoc - zLoc != 1) // 마동석의 어그로가 더 높지만 인접하지 않을 때 마동석 쪽으로 움직임.
 					++zLoc;
 			}
 		}
@@ -216,9 +315,9 @@ int main(void)
 			}
 		}
 
-	// 3.3 마동석 움직임 입력 및 위치 최신화 및 열차 재출력.
-		
-		mMove = getMmove(zLoc,mLoc);
+		// 3.3 마동석 움직임 입력 및 위치 최신화 및 열차 재출력.
+
+		mMove = getMmove(zLoc, mLoc);
 		mLoc -= mMove;
 		printf("\n\n");
 		printTrain(cLoc, zLoc, mLoc, length);
@@ -226,12 +325,12 @@ int main(void)
 
 
 
-    //3.4 마동석 위치 변화 및 어그로 스테미너 변화 출력
+		//3.4 마동석 위치 변화 및 어그로 스테미너 변화 출력
 		if (mMove == MOVE_LEFT)
 		{
 			++mAggro;
 			mAggro = verifyAggro(mAggro);
-			printf("madongseok %d -> %d",preMloc,mLoc);
+			printf("madongseok %d -> %d", preMloc, mLoc);
 		}
 		else if (mMove == MOVE_STAY)
 		{
@@ -250,7 +349,7 @@ int main(void)
 		}
 
 		premAggro = mAggro, precAggro = cAggro;
-		
+
 		//4. 행동 phase 
 
 		// 4.1 시민 행동 phase
@@ -264,8 +363,8 @@ int main(void)
 			printf("citidens does nothing\n");
 		}
 
-	
-	
+
+
 
 		//4.2 좀비행동
 
@@ -281,7 +380,7 @@ int main(void)
 				else// 마동석 공격
 				{
 					--stm;
-					printf("Zombie attacked madongseok (aggro: %d vs %d, madongseok stamina: %d->%d)\n",cAggro,mAggro,preStm,stm);
+					printf("Zombie attacked madongseok (aggro: %d vs %d, madongseok stamina: %d->%d)\n", cAggro, mAggro, preStm, stm);
 					if (stm == STM_MIN)
 					{
 						printf("GAME OVER!. madongseok killed by zombie");
@@ -312,14 +411,14 @@ int main(void)
 		{
 			printf("zombie attacked nobody\n");
 		}
-	
+
 		preStm = stm;
 
 		//4.3 마동석 행동
-	
+
 		if (mLoc - zLoc != 1) // 인접하지 않다면
 		{
-			while (1) 
+			while (1)
 			{
 				printf("madongseok action (0.rest 1.provoke)>> ");
 				scanf_s("%d", &mAction);
@@ -362,7 +461,7 @@ int main(void)
 		{
 			mAggro = AGGRO_MAX;
 			printf("madongseok provoked zombie...\n");
-			printf("madongseok: %d (aggro %d -> %d, stamina: %d)\n", mLoc, premAggro, mAggro,stm);
+			printf("madongseok: %d (aggro %d -> %d, stamina: %d)\n", mLoc, premAggro, mAggro, stm);
 			zombiePull = 0;
 			break;
 		}
@@ -372,13 +471,13 @@ int main(void)
 			mAggro = verifyAggro(mAggro);
 			--stm;
 			stm = verifyStamina(stm);
-			
+
 			int randomNum = rand() % 100;
 			if (100 - prob > randomNum)
 			{
 				printf("madongseok pulled zombie... Next turn, it can't move\n");
 				zombiePull = 1;
-				
+
 			}
 			else
 			{
@@ -392,257 +491,14 @@ int main(void)
 
 		printf("\n\n\n");
 		//4.5 이전 값 저장 변수에 현재 값 대입
-		
-		preMloc = mLoc, preCloc = cLoc;
+
+		preMloc = mLoc, preCloc = cLoc, preZloc = zLoc;
 		premAggro = mAggro, precAggro = cAggro;
 		preStm = stm;
 
-		
+
 	}
 
 	return 0;
-
-//	int h = 0; // (c가 이동하기 위해 공백을 줄이는 변수 h을 선언)
-//	int z = 0; // (좀비가 이동하기 위해 공백을 줄이는 변수 z를 선언 
-//	int turn = 0; // 좀비가 움직이는 턴 확인을 위한 변수
-//
-//	int cSpos = lenght - 6, zSpos = lenght - 3; // 시민과 좀비의 초기위치 저장 변수
-//	int cLoc = cSpos, zLoc = zSpos; // 현재의 시민 위치와 좀비 위치 저장변수
-//	int preCloc = cSpos, preZloc = zSpos; // 이전 턴의 시민과 좀비위치 저장변수
-//
-//
-//	// 4. 4초마다 시민이동 좀비이동 후 열차상태 출력 및 시민, 좀비상태 출력
-//
-//
-//	while (1)
-//
-//	{
-//		++turn; // 1턴부터 시작. 
-//		int rNum = rand() % 100; // 0~99까지의 난수를 저장하는 변수
-//
-//		// 4.1 시민, 좀비가 이동할 때 마다 h , z 값 증가
-//
-//		if (100 - prob > rNum) // 100-p의 확률에 해당될 때 즉, 시민이 움직일 때
-//		{
-//			++h; // h값 +1
-//		}
-//
-//		else // p의 확률 즉, 좀비가 움직일 떄
-//		{
-//			if (turn % 2 != 0) // 좀비가 움직이는 턴에 p%의 확률이 해당된다면.	 
-//			{
-//				++z; //z값 +1
-//			}
-//			// 좀비가 움직이지 않는 턴이면 어떠한 값의 변화 없음
-//		}
-//
-//		// 4.2 변화된 h , z의 값을 이용해 변화된 위치의 C 와 Z 출력
-//
-//		for (int i = 0; i < 3; ++i)
-//
-//		{
-//
-//			if (i == 1)
-//			{
-//
-//				putchar('#');
-//				for (int k = 0; k < (lenght - 7 - h); ++k) // 원래의 공백에서 사람이 움직인 만큼의 공백을 빼고 출력
-//				{
-//					putchar(' ');
-//				}
-//				putchar('C');
-//				for (int a = 0; a < 2 + h - z; ++a) // 사람과 좀비의 초기 공백은 2이고 사람이 움직인 만큼 공백을 더 출력하고 좀비가 움직인 만큼 공백을 덜 출력하여 사람과 좀비의 간격조절. 
-//				{
-//					printf(" ");
-//				}
-//
-//				putchar('Z');
-//				for (int j = 0; j < z; ++j) // 좀비가 왼쪽으로 움직인 만큼 마동석과의 거리(공백) 증가			
-//				{
-//					printf(" ");
-//				}
-//				putchar('M');
-//				putchar('#');
-//				putchar('\n');
-//
-//
-//			}
-//			else
-//			{
-//				for (int j = 0; j < lenght; ++j)
-//				{
-//					putchar('#');
-//				}
-//
-//				putchar('\n');
-//			}
-//
-//		}
-//		putchar('\n');
-//
-//		//4.3 이전위치와 현재위치를 통해 C와 Z의 위치변화 출력
-//
-//		zLoc = zSpos - z; // 현재위치 최신화 (초기 위치에서 늘어난 h와 z 의 값을 빼면 현재위치임)
-//		cLoc = cSpos - h;
-//
-//
-		//if (cLoc != preCloc) //사람이 움직였으면
-		//{
-
-		//	printf("citizen: %d -> %d\n", preCloc, cLoc);
-
-		//}
-
-		//else // 사람이 움직이지 않았으면
-		//{
-
-		//	printf("citizens : stay %d\n", cLoc);
-
-
-		//}
-
-		//if (zLoc == preZloc) //  좀비가 움직이지 않았을 때
-		//{
-		//	if (turn % 2 == 0) // 좀비가 움직이지 못하는 턴이면
-		//	{
-
-		//		printf("zombie stay %d (cannot move)\n", zLoc);
-		//		putchar('\n');
-		//	}
-		//	else // 좀비가 움직일 수 있는 턴에 못 움직이면
-		//	{
-
-		//		printf("zombie stay %d\n", zLoc);
-		//		putchar('\n');
-		//	}
-		//}
-		//else // 좀비가 움직였으면
-		//{
-		//	printf("zombie: %d -> %d\n", preZloc, zLoc);
-		//	putchar('\n');
-		//}
-
-//		preCloc = cLoc; // 다음 턴에 사용하기 위해 이전위치 변수 최신화 
-//		preZloc = zLoc;
-//
-//
-//		//4.4 게임이 끝나는 조건을 통한 아웃트로 작성
-//
-//		if (cLoc - zLoc == -1) // 좀비의 위치가 사람의 위치 옆에 도달 했을 때 아웃트로 
-//		{
-//			printf("\n\n\n\n\n");
-//			printf("GAME OVER!\nCitizen(s) has(have) been attacked by a zombie\n\n\n\n\n\n\n");
-//			Sleep(2000);
-//			break;
-//		}
-//
-//		if (cLoc == 1) //  사람의 기차의 첫번 째 칸에 도달했을 때 아웃트로
-//		{
-//			printf("\n\n\n\n\n");
-//			printf("SUCCESS! citizen(s) escaped to the next train\n\n\n\n\n\n\n");
-//			Sleep(2000);
-//			break;
-//		}
-//
-//		Sleep(4000);
-//	}
-//
-//
-// 	return 0;
- }
-
- void AsciiArt(void)  //  ASCII art를 이용한 인트로 출력함수
- {
-	 printf("                 ()\n");
-	 printf("                 ()\n");
-	 printf("                 ()       ____.______._____\n");
-	 printf("	      .--.  ---- - | _ - a:f - |\n");
-	 printf("	  __ || ___ | [_]| |. | #  | .[].[].[].. |\n");
-	 printf("	   o)__ | _ | .. |= | _ | -| ___________ |\n");
-	 printf("	  __ < (__(*)_(*)_~_____~(*)____(*)_____\n");
-	 printf("          ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~\n");
-
-	 printf("                 TRAIN TO BUSAN\n");
-	 printf("\n\n\n\n");
- }
-
- int getLenght(void)      //  기차길이 입력을 받는 함수
- {
-	 int nInput;
-	 while (1)
-	 {
-		 printf("train lenght(15~50)>>");
-		 scanf_s("%d", &nInput);
-		 if (nInput >= LEN_MIN && nInput <= LEN_MAX)
-		 {
-			 break;
-		 }
-
-	 }
-	 return nInput;
- }
-
- int getStamina(void)      // stm 입력을 받는 함수
- {
-	 int nInput;
-	 while (1)
-	 {
-		 printf("madongseok stamina(0~5)>>");
-		 scanf_s("%d", &nInput);
-		 if (nInput >= STM_MIN && nInput <= STM_MAX)
-		 {
-			 break;
-		 }
-
-	 }
-	 return nInput;
- }
-
- int getProb(void)      //  확률 입력을 받는 함수
- {
-	 int nInput;
-	 while (1)
-	 {
-		 printf("percentile probability 'p'(10~90) >> ");
-		 scanf_s("%d", &nInput);
-		 if (nInput >= PROB_MIN && nInput <= PROB_MAX)
-		 {
-			 break;
-		 }
-
-	 }
-	 return nInput;
- }
-
- void printTrain(int cLoc, int zLoc, int mLoc, int length) // 열차상태 출력함수
- {
-
-	 for (int k = 0; k < length + 1; k++)
-	 {
-		 g_trainList[k] = ' ';
-	 }
-	 g_trainList[length] = '\0';
-
-	 for (int i = 0; i < 3; i++)
-	 {
-		 if (i == 1)
-		 {
-			 g_trainList[0] = '#';
-			 g_trainList[length - 1] = '#';
-			 g_trainList[cLoc] = 'C';
-			 g_trainList[zLoc] = 'Z';
-			 g_trainList[mLoc] = 'M';
-			 printf("%s\n", g_trainList);
-		 }
-
-		 else
-		 {
-			 for (int j = 0; j < length; j++)
-			 {
-				 putchar('#');
-			 }
-			 putchar('\n');
-		 }
-	 }
- }
+}
 
